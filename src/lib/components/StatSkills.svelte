123d4e538ea43fd06a1321173487c4057b210618
Chor <97508567+tanch-p@users.noreply.github.com>
2023-02-27 01:40:13 +0900

    fixed statSkills value

--- src/lib/components/StatSkills.svelte
+++ src/lib/components/StatSkills.svelte
@@ -3,7 +3,7 @@
 	import translations from '$lib/translations.json';
 	import enemySkills from '$lib/data/enemy/enemy_skills.json';
 	export let skills: string[], stat: string, statValue: number, language: string;
-    
+
 	$: separator = language === 'en' ? '/' : '・';
 	$: skillsToParse = skills
 		.map((skillName) => {
@@ -14,7 +14,7 @@
 				if (fixed_value) {
 					value = fixed_value;
 				} else {
-					value * (multiplier ?? 1) + (fixed_inc ?? 0);
+					value = value * (multiplier ?? 1) + (fixed_inc ?? 0);
 				}
 				return { suffix, value, hits: hits ?? 0, dmgEle: dmg_element ?? '' };
 			}
