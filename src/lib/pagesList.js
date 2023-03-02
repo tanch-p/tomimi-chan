b21674e330fb84aee5d5992477934ea9f0c532d0
Chor <97508567+tanch-p@users.noreply.github.com>
2023-03-02 22:31:42 +0900

    moved pagesToRender to its own file

---
+++ src/lib/pagesList.js
@@ -0,0 +1,17 @@
+import ISStagesList from './data/stages/is_stages_list.json' assert { type: 'json' };
+const LANGUAGES = ['en', 'jp'];
+const PAGES = ['about', 'credits', 'mizuki', 'phantom'];
+
+export const pagesToRender = LANGUAGES.reduce(
+	(acc, language) => {
+		for (const page of PAGES) {
+			acc.push(`/${language}/${page}`);
+		}
+		for (const stage of ISStagesList) {
+			const stageName = stage.code + '_' + stage[`name_${language}`];
+			acc.push(`/${language}/stages/${stageName}`);
+		}
+		return acc;
+	},
+	['/']
+);
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import ISStagesList from './src/lib/data/stages/is_stages_list.json' assert { type: 'json' };
-
-const LANGUAGES = ['en', 'jp'];
-const PAGES = ['about', 'credits', 'mizuki', 'phantom'];
+import { pagesToRender } from './src/lib/pagesList.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,19 +11,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		prerender: {
-			entries: LANGUAGES.reduce(
-				(acc, language) => {
-					for (const page of PAGES) {
-						acc.push(`/${language}/${page}`);
-					}
-					for (const stage of ISStagesList) {
-						const stageName = stage.code + '_' + stage[`name_${language}`];
-						acc.push(`/${language}/stages/${stageName}`);
-					}
-					return acc;
-				},
-				['/']
-			)
+			entries: pagesToRender
 		}
 	}
 };
