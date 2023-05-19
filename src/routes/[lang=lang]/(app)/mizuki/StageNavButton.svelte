84c7ad5210f8c7d848f3f403f3e14f6147d425a9
Chor <97508567+tanch-p@users.noreply.github.com>
2023-05-19 22:29:07 +0900

    hid update2 stages for eng/jp

--- src/routes/[lang=lang]/(app)/mizuki/StageNavButton.svelte
+++ src/routes/[lang=lang]/(app)/mizuki/StageNavButton.svelte
@@ -2,15 +2,19 @@
 	import stageList from '$lib/data/stages/is_stages_list.json';
 	export let stageName: string, language: string;
 
+	const update2Stages = ['互助', '饥渴', '瞻前顾后', '铳与秩序', '余烬方阵', '人之光辉'];
+
 	const stageInfo = stageList.find((stage) => stage.name_zh === stageName);
 	if (!stageInfo) {
 		throw new Error(`${stageName} is not found!`);
 	}
 	const stageUrl = stageInfo.code + '_' + (stageInfo[`name_${language}`] || stageInfo['name_zh']);
+
+	$: hidden = language !== "zh" && update2Stages.includes(stageName)
 </script>
 
 <a href={`/${language}/stages/${stageUrl}`} class="hover:text-sky-400">
-	<div class={`py-2 my-0.5 sm:my-1 text-center hover:backdrop-brightness-50`}>
+	<div class={`py-2 my-0.5 sm:my-1 text-center hover:backdrop-brightness-50 ${hidden ? "opacity-0 hover:opacity-100" : ""}`}>
 		{stageName}
 	</div>
 </a>
