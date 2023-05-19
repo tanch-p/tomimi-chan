914555ed87ac7fbbb9a4a3ba4fb5e8cf0c32d05f
Chor <97508567+tanch-p@users.noreply.github.com>
2023-05-20 00:28:32 +0900

    added resize to relics src

--- src/lib/components/RelicDiv.svelte
+++ src/lib/components/RelicDiv.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let relic, language: string, rogueTopic: string, selectedRelics;
 
-	const src = `https://res.cloudinary.com/dbqz7mebk/image/upload/v1681056192/tomimi.dev/relics/${relic.img}.webp`;
+	const src = `https://res.cloudinary.com/dbqz7mebk/image/upload/c_fit,w_100/v1681056192/tomimi.dev/relics/${relic.img}.webp`;
 	$: name = relic[`name_${language}`] || relic[`name_zh`];
 	$: tooltip = relic[`tooltip_${language}`] || relic[`tooltip_zh`];
 
