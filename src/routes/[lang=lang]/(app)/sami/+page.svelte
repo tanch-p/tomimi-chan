edff348cb8f57b33e48a9180c023b67635f4f8c8
Chor <97508567+tanch-p@users.noreply.github.com>
2023-07-13 00:15:46 +0900

    progress commit

---
+++ src/routes/[lang=lang]/(app)/sami/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import MizukiNav from './MizukiNav.svelte';
+	import type { PageData } from './$types';
+	import banner_mizuki from '$lib/images/banner_mizuki_800px.webp';
+	import translations from '$lib/translations.json';
+	import TogglePanel from '$lib/components/TogglePanel.svelte';
+	import HowToUse from '$lib/components/HowToUse.svelte';
+
+	export let data: PageData;
+	$: language = data.language;
+</script>
+
+<svelte:head>
+	<title>{translations[language].rogue_mizuki} / {translations[language].title_post}</title>
+	<meta name="description" content={translations[language].title_post} />
+	<meta property="og:description" content={translations[language].title_post} />
+	<meta property="og:title" content={translations[language].rogue_mizuki} />
+	<meta property="og:url" content={`https://tomimi.dev/${language}/mizuki`} />
+</svelte:head>
+
+<div class="bg-neutral-800 min-h-screen">
+	<div class="pb-24 max-w-4xl mx-auto">
+		<div class="py-4">
+			<img
+				class="select-none mx-auto py-2"
+				src={banner_mizuki}
+				alt={translations[language].rogue_mizuki}
+			/>
+		</div>
+		<div class="space-y-6">
+			<TogglePanel title={translations[language].how_to_use}>
+				<HowToUse {language} rogueTopic={'rogue_mizuki'} />
+			</TogglePanel>
+			<TogglePanel title={translations[language].related_tools}>
+				<ul class="text-lg pt-2 pb-6">
+					<li class="list-disc list-inside">
+						<a class="text-sky-500 hover:underline" href="/{language}/laoli">
+							{translations[language].laoli_title}
+						</a>
+					</li>
+				</ul>
+			</TogglePanel>
+			<TogglePanel title={translations[language].stage_nav} isOpen={true}>
+				<MizukiNav {language} />
+			</TogglePanel>
+		</div>
+	</div>
+</div>
---
+++ src/routes/[lang=lang]/(app)/sami/StageNavButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import stageList from '$lib/data/stages/is_stages_list.json';
+	export let stageName: string, language: string;
+
+	const update2Stages = ['互助', '饥渴', '瞻前顾后', '铳与秩序', '余烬方阵', '人之光辉'];
+
+	const stageInfo = stageList.find((stage) => stage.name_zh === stageName);
+	if (!stageInfo) {
+		throw new Error(`${stageName} is not found!`);
+	}
+	$: stageUrl = stageInfo.code + '_' + (stageInfo[`name_${language}`] || stageInfo['name_zh']);
+
+	$: hidden = language !== 'zh' && update2Stages.includes(stageName);
+
+	$: name = stageInfo[`name_${language}`] || stageInfo['name_zh'];
+</script>
+
+<a data-sveltekit-preload-code="eager" href={`/${language}/stages/${stageUrl}`} class="hover:text-sky-400">
+	<div
+		class={`py-2 my-0.5 sm:my-1 text-center hover:backdrop-brightness-50 ${
+			hidden ? 'opacity-0 hover:opacity-100' : ''
+		}`}
+	>
+		{name.replaceAll('_', ' ')}
+	</div>
+</a>
+
+<style>
+</style>
