52f29e7e20e1d6a30a40d9424ff1bfb2e3f84f89
tanch-p <97508567+tanch-p@users.noreply.github.com>
2023-07-14 10:43:25 +0800

    added skills for new enemies

--- src/routes/[lang=lang]/(app)/sami/+page.svelte
+++ src/routes/[lang=lang]/(app)/sami/+page.svelte
@@ -28,24 +28,24 @@
 			/>
 		</div>
 		<div class="space-y-6">
-			<TogglePanel title="工程进度">
+			<TogglePanel title="开发进度">
 				<div class="px-2">
-					<ul class="py-2">做好了</ul>
 					<ul class="mt-2">
 						验证中
 						<li class="list-disc list-inside">敌人数量</li>
-						<li class="list-disc list-inside">{translations[language].how_to_use_elite_toggle}</li>
+						<li class="list-disc list-inside">敌人属性加成</li>
 					</ul>
 					<ul class="mt-2">
 						开发中
-						<li class="list-disc list-inside">敌人数量</li>
-						<li class="list-disc list-inside">{translations[language].how_to_use_elite_toggle}</li>
+						<li class="list-disc list-inside">敌人技能详细</li>
+						<li class="list-disc list-inside">坍缩</li>
+						<li class="list-disc list-inside">收藏品</li>
 					</ul>
 				</div>
 			</TogglePanel>
-			<TogglePanel title={translations[language].how_to_use}>
+			<!-- <TogglePanel title={translations[language].how_to_use}>
 				<HowToUse {language} rogueTopic={'rogue_mizuki'} />
-			</TogglePanel>
+			</TogglePanel> -->
 			<TogglePanel title={translations[language].stage_nav} isOpen={true} >
 				<SamiNavTemp {language} />
 			</TogglePanel>
