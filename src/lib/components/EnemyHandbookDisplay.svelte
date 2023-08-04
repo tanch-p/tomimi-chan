6d611c8302b9af01032fc96e65cb2f28df5e49f3
tanch-p <97508567+tanch-p@users.noreply.github.com>
2023-08-04 17:57:12 +0800

    progress commit

--- src/lib/components/EnemyHandbookDisplay.svelte
+++ src/lib/components/EnemyHandbookDisplay.svelte
@@ -9,57 +9,76 @@
 	const statKeys = ['hp', 'ms', 'atk', 'aspd', 'def', 'res'];
 </script>
 
-<div class="flex flex-col">
-	{#each enemies as enemy, index (enemy.key)}
-		<div>
-			<div class="flex gap-x-2">
-				{#each enemy.type.filter((ele) => ele !== 'NORMAL') as type}
-					<p class="whitespace-nowrap">{translations[language].types[type]}</p>
-				{/each}
-			</div>
-			<div class="flex items-center">
-				<p>{enemy.id}</p>
-				{#if enemy.overwritten}
-					<p>Sp.</p>
-				{/if}
-				<p>{enemy[`name_${language}`]}</p>
-			</div>
-			<div class="grid grid-cols-[100px_auto]">
-				<div class="flex flex-col gap-y-2">
-					<p>ATK type Dmg Element</p>
+<div class="grid grid-cols-[100px_auto]">
+	<div class="flex flex-wrap gap-2.5 px-2.5 h-max sticky top-20">
+		{#each enemies as enemy}
+			<a href={`#${enemy.key}`}>
+				<div class="relative">
+					<p class="absolute bg-black px-1 text-xs">{enemy.id}</p>
 					<img
 						class="select-none"
 						src={enemy.img}
-						height="75px"
-						width="75px"
+						height="60px"
+						width="60px"
 						decoding="async"
 						alt={enemy.id}
 					/>
-					<div>
-						I {translations[language].table_headers.weight}
-						{enemy.stats[0].weight}
-					</div>
 				</div>
-				<div>
-					{#if enemy.lifepoint !== 1}
-						<div class="flex items-center justify-between">
-							lifepoint icon {enemy.stats[0].lifepoint}
-						</div>
+			</a>
+		{/each}
+	</div>
+	<div class="flex flex-col">
+		{#each enemies as enemy, index (enemy.key)}
+			<div id={enemy.key} class="scroll-mt-16">
+				<div class="flex gap-x-2">
+					{#each enemy.type.filter((ele) => ele !== 'NORMAL') as type}
+						<p class="whitespace-nowrap">{translations[language].types[type]}</p>
+					{/each}
+				</div>
+				<div class="flex items-center">
+					<p>{enemy.id}</p>
+					{#if enemy.overwritten}
+						<p>Sp.</p>
 					{/if}
-					<div class="grid grid-cols-2 gap-2">
-						{#each statKeys as statKey}
-							<div class="flex items-center">
-								icon {translations[language].table_headers[statKey]}
-								{enemy.stats[0][statKey]}
+					<p>{enemy[`name_${language}`]}</p>
+				</div>
+				<div class="grid grid-cols-[100px_auto]">
+					<div class="flex flex-col gap-y-2">
+						<p>ATK type Dmg Element</p>
+						<img
+							class="select-none"
+							src={enemy.img}
+							height="75px"
+							width="75px"
+							decoding="async"
+							alt={enemy.id}
+						/>
+						<div>
+							I {translations[language].table_headers.weight}
+							{enemy.stats[0].weight}
+						</div>
+					</div>
+					<div>
+						{#if enemy.lifepoint !== 1}
+							<div class="flex items-center justify-between">
+								lifepoint icon {enemy.stats[0].lifepoint}
 							</div>
-						{/each}
+						{/if}
+						<div class="grid grid-cols-2 gap-2">
+							{#each statKeys as statKey}
+								<div class="flex items-center">
+									icon {translations[language].table_headers[statKey]}
+									{enemy.stats[0][statKey]}
+								</div>
+							{/each}
+						</div>
 					</div>
 				</div>
+				<div class="flex flex-col">
+					<HandbookAbilities {enemy} {language} {statMods} {specialMods} />
+					<StatusImmune {enemy} {specialMods} {language} mode="handbook" />
+				</div>
 			</div>
-			<div class="flex flex-col">
-				<HandbookAbilities {enemy} {language} {statMods} {specialMods} />
-				<StatusImmune {enemy} {specialMods} {language} mode="handbook" />
-			</div>
-		</div>
-	{/each}
+		{/each}
+	</div>
 </div>
