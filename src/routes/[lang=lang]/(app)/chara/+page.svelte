f6efb002fd9a0b72f0d472d26f04298431e381b5
tanch-p <97508567+tanch-p@users.noreply.github.com>
2023-09-15 14:32:45 +0800

    added chara page skeleton

---
+++ src/routes/[lang=lang]/(app)/chara/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import translations from '$lib/translations.json';
+
+	export let data: PageData;
+	$: language = data.language;
+</script>
+
+<svelte:head>
+	<title>{translations[language].chara_page}</title>
+	<meta name="description" content={translations[language].title_post} />
+	<meta property="og:description" content={translations[language].title_post} />
+</svelte:head>
+
+
