2ff9b81e0ae5b5042ad034a6f7ad2973ad3a300c
Chor <97508567+tanch-p@users.noreply.github.com>
2024-04-09 17:17:16 +0800

    add results count to chara display

--- src/lib/components/CharaDisplay.svelte
+++ src/lib/components/CharaDisplay.svelte
@@ -4,14 +4,17 @@
 	// $:console.log(characters);
 </script>
 
-{#if characters.length === 0}
-	<p>None</p>
-{:else}
-	<div class="grid grid-cols-4 md:grid-cols-8 max-w-4xl mx-auto">
-		{#each characters as chara}
-			<div>
-				{chara.appellation}
-			</div>
-		{/each}
-	</div>
-{/if}
+<div class="max-w-4xl mx-auto">
+	{#if characters.length === 0}
+		<p class="text-center">No results found</p>
+	{:else}
+		<p>{characters.length} results returned</p>
+		<div class="grid grid-cols-4 md:grid-cols-8 mt-4">
+			{#each characters as chara}
+				<div>
+					{chara.appellation}
+				</div>
+			{/each}
+		</div>
+	{/if}
+</div>
