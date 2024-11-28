4c072399423333a093895cf416ad8000cd5323d3
Chor <97508567+tanch-p@users.noreply.github.com>
2024-11-28 22:00:27 +0900

    add how to use images

--- src/lib/components/HowToUse.svelte
+++ src/lib/components/HowToUse.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { Language } from '$lib/types';
 	import translations from '$lib/translations.json';
+	import floorHelp from '$lib/images/others/floor_title_help.webp';
+	import relicHelp from '$lib/images/others/relic_help.webp';
 
 	export let language: Language, rogueTopic: string;
 </script>
@@ -9,24 +11,27 @@
 	<p>
 		{translations[language].how_to_use_main}
 	</p>
-	<ul class="mt-2">
-		{translations[language].how_to_use_ui_list}
-
-		<li class="list-disc list-inside">{translations[language].how_to_use_relic}</li>
-		<li class="list-disc list-inside">{translations[language].how_to_use_elite_toggle}</li>
+	<p class="mt-2">{translations[language].how_to_use_ui_list}</p>
+	<ul class="list-disc pl-4 space-y-1">
+		<li>
+			{translations[language].how_to_use_relic}
+			<img src={relicHelp} alt="relic help" class="pl-0.5" />
+		</li>
+		<li>{translations[language].how_to_use_elite_toggle}</li>
 		{#if rogueTopic !== 'rogue_phantom'}
-			<li class="list-disc list-inside">{translations[language].how_to_use_diff}</li>
-			<li class="list-disc list-inside">{translations[language].how_to_use_floor}</li>
+			<li>{translations[language].how_to_use_diff}</li>
+			<li>{translations[language].how_to_use_floor}</li>
+			<img src={floorHelp} alt="floor help" class="pl-0.5" />
 		{/if}
 		{#if rogueTopic === 'rogue_mizuki'}
-			<li class="list-disc list-inside">{translations[language].how_to_use_seaborn}</li>
-			<li class="list-disc list-inside">{translations[language].how_to_use_mission}</li>
+			<li>{translations[language].how_to_use_seaborn}</li>
+			<li>{translations[language].how_to_use_mission}</li>
 		{:else if rogueTopic === 'rogue_sami'}
-			<li class="list-disc list-inside">{translations[language].how_to_use_chaos}</li>
+			<li>{translations[language].how_to_use_chaos}</li>
 		{:else if rogueTopic === 'rogue_phantom'}
-			<li class="list-disc list-inside">{translations[language].how_to_use_hallu}</li>
+			<li>{translations[language].how_to_use_hallu}</li>
 		{:else if rogueTopic === 'rogue_skz'}
-			<li class="list-disc list-inside">{translations[language].how_to_use_sarkaz}</li>
+			<li>{translations[language].how_to_use_sarkaz}</li>
 		{/if}
 	</ul>
 </div>
