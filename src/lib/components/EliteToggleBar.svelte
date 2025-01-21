62ee6e62a505e7dfc31c516a80dff647c8f5a9e9
Chor <97508567+tanch-p@users.noreply.github.com>
2025-01-21 18:00:22 +0800

    update

---
+++ src/lib/components/EliteToggleBar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import combat_icon from '$lib/images/is/combat_icon.webp';
+
+	export let stageId: string,
+		eliteMode,
+		combatOpsColor,
+		eliteOpsColor,
+		getEliteIcon,
+		updateEliteMods;
+</script>
+
+<div class="grid grid-cols-2 font-bold text-lg text-gray-700 select-none">
+	<button
+		id="normal-toggle"
+		class={`flex justify-center items-center py-1 ${combatOpsColor} ${
+			!$eliteMode ? 'text-gray-900' : 'opacity-30'
+		}`}
+		on:click={() => updateEliteMods(false)}
+	>
+		<img src={combat_icon} width="50px" decoding="async" loading="lazy" alt="combat ops" class="" />
+	</button>
+	<button
+		id="elite-toggle"
+		class={`flex justify-center items-center ${eliteOpsColor} ${
+			$eliteMode ? 'text-black' : 'opacity-30'
+		}`}
+		on:click={() => updateEliteMods(true)}
+	>
+		<img
+			src={getEliteIcon(stageId)}
+			width="50px"
+			decoding="async"
+			loading="lazy"
+			alt="elite ops"
+			class=""
+		/>
+	</button>
+</div>
