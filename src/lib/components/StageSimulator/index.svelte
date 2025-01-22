31291f35fb08b2c9ef36850748ff6e1e16587272
Chor <97508567+tanch-p@users.noreply.github.com>
2025-01-22 10:37:46 +0800

    update

--- src/lib/components/StageSimulator/index.svelte
+++ src/lib/components/StageSimulator/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { Game } from './objects/Game';
 
-	export let waveData;
+	export let waveData,mapConfig, enemies;
 
 	let count, waves, canvasElement: HTMLCanvasElement;
 
@@ -9,10 +10,19 @@
 	$: waves = waveData.waves;
 
 	onMount(() => {
-		// const game = new Game(config, enemies, canvasElement);
+		const game = new Game(mapConfig, enemies, waves, canvasElement);
 	});
 </script>
 
 <div>
 	<canvas bind:this={canvasElement} />
 </div>
+
+<style>
+	canvas {
+	  width: 100%;
+	  height: 100%;
+	  display: block;
+	  margin: 0 auto;
+	}
+  </style>
