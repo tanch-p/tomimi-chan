9084539a4835f3415623cf7b03574a7d5903b070
Chor <97508567+tanch-p@users.noreply.github.com>
2025-02-27 17:21:38 +0800

    update

--- src/lib/components/MediaQuery.svelte
+++ src/lib/components/MediaQuery.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import { dev } from '$app/environment';
 	import { onMount } from 'svelte';
 
 	let layout;
 	onMount(() => {
-		const mql = dev
-			? window.matchMedia('(max-width:768px)')
-			: window.matchMedia('(pointer: coarse)');
+		const mql = window.matchMedia('(max-width:768px) or (pointer: coarse)');
 		layout = mql.matches ? 'mobile' : 'pc';
 		mql.addEventListener('change', screenTest);
 		return () => {
