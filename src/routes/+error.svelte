a3f6e97bbdb5ab5abacd6f4522d77714cca78f08
Chor <97508567+tanch-p@users.noreply.github.com>
2025-03-09 17:46:46 +0900

    added message to error page to help debugging

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -20,7 +20,7 @@
 			}, redirectTime);
 		}
 	});
-	$: message = $page.error?.message;
+	const message = $page.error?.message;
 </script>
 
 <svelte:head>
@@ -34,6 +34,7 @@
 				The page you requested could not be found
 			{:else}
 				An unexpected error occured while loading the page. <br />
+				{message} <br/>
 				If this error persists, please kindly inform me on discord xiiao#0613
 			{/if}
 			<br /><br />
