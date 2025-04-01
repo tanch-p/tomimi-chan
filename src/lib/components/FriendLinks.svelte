90623b74274b353734e2b3e3c95c7216df4392f5
Chor <97508567+tanch-p@users.noreply.github.com>
2025-04-01 17:21:58 +0800

    hide friendlink

--- src/lib/components/FriendLinks.svelte
+++ src/lib/components/FriendLinks.svelte
@@ -25,7 +25,7 @@
 	};
 </script>
 
-<div class="w-full md:w-[600px] bg-[#363636] shadow-lg md:shadow-xl pt-2 sm:py-3 px-2 sm:px-6">
+<!-- <div class="w-full md:w-[600px] bg-[#363636] shadow-lg md:shadow-xl pt-2 sm:py-3 px-2 sm:px-6">
 	<div class="flex items-center justify-center">
 		<span>
 			<img src={friendlink} alt={'友情链接'} width="31" height="29" />
@@ -54,7 +54,7 @@
 			{tts[language].desc}
 		</p>
 	</div>
-</div>
+</div> -->
 
 <style>
 	p {
