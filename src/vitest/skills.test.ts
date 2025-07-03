09f97711a88f734dd3d9069d1999647a63c29027
Chor <97508567+tanch-p@users.noreply.github.com>
2025-07-03 17:54:42 +0800

    added test to check all enemy keys

---
+++ src/vitest/skills.test.ts
@@ -0,0 +1,51 @@
+import { describe, it, expect } from 'vitest';
+import enemyDB from '../lib/data/enemy/enemy_database.json' assert { type: 'json' };
+import enemySkills from '../lib/data/enemy/enemy_skills.json' assert { type: 'json' };
+import trapsDB from '../lib/data/trap/traps.json' assert { type: 'json' };
+import trapSkills from '../lib/data/trap/traps_skills.json' assert { type: 'json' };
+
+describe('Skill key check', () => {
+	it('enemy skills', () => {
+		const undefinedKeyList: [string, string][] = [];
+
+		for (const [key, enemy] of Object.entries(enemyDB)) {
+			for (const stats of enemy['stats']) {
+				for (const skill of stats['traits']) {
+					if (!enemySkills[skill.key]) {
+						undefinedKeyList.push([key, skill.key]);
+					}
+				}
+				for (const form of stats['special']) {
+					for (const skill of form) {
+						if (!enemySkills[skill.key]) {
+							undefinedKeyList.push([key, skill.key]);
+						}
+					}
+				}
+			}
+		}
+
+		expect(undefinedKeyList.length).toBe(0);
+	});
+	it('trap skills', () => {
+		const undefinedKeyList: [string, string][] = [];
+
+		for (const [key, trap] of Object.entries(trapsDB)) {
+			const skills = [...trap['skills'], ...trap['talents'], ...trap['special']];
+			for (const skillKey of skills) {
+				if (typeof skillKey === 'object') {
+					for (const formSkillKey of skillKey) {
+						// case trap_762_skztxy
+						if (!trapSkills[formSkillKey]) {
+							undefinedKeyList.push([key, formSkillKey]);
+						}
+					}
+				} else if (!trapSkills[skillKey]) {
+					undefinedKeyList.push([key, skillKey]);
+				}
+			}
+		}
+
+		expect(undefinedKeyList.length).toBe(0);
+	});
+});
