93ace78b0c74783d7e9269a065a64cd546561960
Chor <97508567+tanch-p@users.noreply.github.com>
2025-07-16 12:02:10 +0800

    update

--- src/params/yan_maps.ts
+++
@@ -1,6 +0,0 @@
-import type { ParamMatcher } from '@sveltejs/kit';
-import validateStage from '$lib/functions/validateStage';
-
-export const match = ((param) => {
-	return !!validateStage(param, 'rogue_yan');
-}) satisfies ParamMatcher;
