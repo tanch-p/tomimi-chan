146de7b926d351126f68bb314cee00bf753a1250
Chor <97508567+tanch-p@users.noreply.github.com>
2025-08-26 14:44:47 +0800

    update

--- src/lib/components/StageSimulator/stores.js
+++ src/lib/components/StageSimulator/stores.js
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 import { GameConfig } from './objects/GameConfig';
 
 export const simMode = writable('wave_normal');
+export const randomSeeds = writable(Array.from(Array(50)).map(() => Math.random()));
--- src/lib/global_stores.js
+++
@@ -1,3 +0,0 @@
-import { writable } from "svelte/store";
-
-export const randomSeeds = writable(Array.from(Array(50)).map(() => Math.random()));
