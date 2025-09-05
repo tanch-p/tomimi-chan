9fc6e25236d8b5169b40d50b6de0770a3b41fcb1
Chor <97508567+tanch-p@users.noreply.github.com>
2025-09-05 10:07:17 +0800

    update contract desc icons

--- src/lib/components/recalRune/ContractDesc.svelte
+++ src/lib/components/recalRune/ContractDesc.svelte
@@ -8,36 +8,68 @@
 
 <div class="w-full bg-[#1b1b1b] max-h-[60vh] md:max-h-full overflow-y-auto">
 	<div class="flex items-center justify-center bg-[#82000e]">
-		{crisis.fixedRuneSeriesName[language] || crisis.fixedRuneSeriesName['zh']}
+		<img src="/images/recalrune/icon_rune_head_essential.webp" alt="essential_icon" />
+		<h6>{crisis.fixedRuneSeriesName[language] || crisis.fixedRuneSeriesName['zh']}</h6>
 	</div>
 	<div class="grid grid-cols-[50px_1fr] gap-y-5 pt-3">
 		{#each $contracts.base.nullGroup as rune}
-			<div>icon</div>
+			<div class="px-1">
+				<div
+					class="flex items-center justify-center rounded w-full py-0.5 mt-0.5 bg-near-white h-min"
+				>
+					<img src="/images/recalrune/icon_rune_essential_fixed.webp" alt="base_fixed" width="16" />
+				</div>
+			</div>
 			<TextParser line={rune.description[language] || rune.description['zh']} />
 		{/each}
 		{#each Object.entries($contracts.base.exclusiveGroups) as [_, runes]}
 			{#each runes as rune}
 				{#if rune.selected}
-					<div>icon</div>
+					<div class="px-1">
+						<div
+							class="flex items-center justify-center rounded w-full py-0.5 mt-0.5 bg-near-white h-min"
+						>
+							<img
+								src="/images/recalrune/icon_rune_essential_exclusive.webp"
+								alt="base_fixed"
+								width="16"
+							/>
+						</div>
+					</div>
 					<TextParser line={rune.description[language] || rune.description['zh']} />
 				{/if}
 			{/each}
 		{/each}
 	</div>
 	<div class="flex items-center justify-center bg-[#82000e] mt-5">
-		{translations[language]['recal.non_essential']}
+		<img src="/images/recalrune/icon_rune_head_rewarding.webp" alt="rewarding_icon" />
+		<h6>{translations[language]['recal.non_essential']}</h6>
 	</div>
-	<div class="grid grid-cols-[100px_1fr] gap-y-5 pt-3">
+	<div class="grid grid-cols-[50px_1fr] gap-y-5 pt-3 pb-4">
 		{#each $contracts.others.nullGroup as rune}
 			{#if rune.selected}
-				<div>icon</div>
+				<div class="px-1">
+					<div
+						class="flex items-center justify-center gap-x-0.5 rounded w-full py-0.5 mt-0.5 bg-near-white h-min"
+					>
+						<img src="/images/recalrune/icon_rune_rewarding.webp" alt="base_fixed" width="16" />
+						<span class="text-[#333] font-semibold">{rune.score}</span>
+					</div>
+				</div>
 				<TextParser line={rune.description[language] || rune.description['zh']} />
 			{/if}
 		{/each}
 		{#each Object.entries($contracts.others.exclusiveGroups) as [_, runes]}
 			{#each runes as rune}
 				{#if rune.selected}
-					<div>icon</div>
+					<div class="px-1">
+						<div
+							class="flex items-center justify-center gap-x-0.5 rounded w-full py-0.5 mt-0.5 bg-near-white h-min"
+						>
+							<img src="/images/recalrune/icon_rune_rewarding.webp" alt="base_fixed" width="16" />
+							<span class="text-[#333] font-semibold">{rune.score}</span>
+						</div>
+					</div>
 					<TextParser line={rune.description[language] || rune.description['zh']} />
 				{/if}
 			{/each}
--- src/lib/components/recalRune/ContractScore.svelte
+++ src/lib/components/recalRune/ContractScore.svelte
@@ -14,12 +14,12 @@
 		on:click={() => contracts.update((v) => clearContracts(v))}
 		class="hover:brightness-150 active:brightness-125 pt-2"
 	>
-		<img src="/images/recalrune/button_clear.webp" class="h-[45px]" />
+		<img src="/images/recalrune/button_clear.webp" class="h-[45px]" alt='clear' />
 	</button>
 	<div class="rounded w-[3px] h-[32px] bg-[#444] ml-2 mr-4" />
 	<div class="flex items-center justify-center pr-4">
-		<img src="/images/recalrune/icon_selected_score.webp" class="h-[45px]" />
+		<img src="/images/recalrune/icon_selected_score.webp" class="h-[45px]" alt='score'  />
 		<span class="ml-2 text-sm text-[#888]">{translations[language]['recal.score']}</span>
-		<p class="w-[40px] text-end text-4xl">{score}</p>
+		<p class="ml-3 w-[40px] text-end text-4xl">{score}</p>
 	</div>
 </div>
